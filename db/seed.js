aa92e414d31d753f4044e8c4f31b371f8e10bbe3
Dezerey Escanuelas <dezerey11@gmail.com>
2021-03-20 09:00:57 -0700

    first commit

---
+++ db/seed.js
@@ -0,0 +1,15 @@
+// import the database collection
+const mongoose = require("./connection");
+
+///// Import Your Models Below /////
+
+///// Do your Database Operations in Below Function/////
+
+const seed = async () => {
+  //------ CODE GOES HERE
+  console.log("seed file");
+  //---------------------
+};
+
+///// Run seed function /////
+seed();
